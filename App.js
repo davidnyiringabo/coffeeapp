6f01fc2db6ae65c7f1dc3dec670b4f6b4595c03c
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-03-07 16:27:02 +0200

    feat: add splash screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { View } from "react-native";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
@@ -11,12 +11,13 @@ import {
   CartScreen,
 } from "./src/screens";
 import { Entypo, Ionicons, Fontisto } from "@expo/vector-icons";
+import {Splash} from "./src/screens/Splash"
 
-SplashScreen.preventAutoHideAsync();
 
 const Tab = createBottomTabNavigator();
-
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [loading, setLoading] = useState(true);
   const [fontsLoaded, fontError] = Font.useFonts({
     "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
   });
@@ -47,70 +48,80 @@ export default function App() {
   };
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={screenOptions}>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View>
-                <Entypo
-                  name="home"
-                  size={24}
-                  color={focused ? "#C67C4E" : "black"}
-                />
-              </View>
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Favorites"
-          component={FavoritesScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View>
-                <Ionicons
-                  name="heart"
-                  size={24}
-                  color={focused ? "#C67C4E" : "black"}
-                />
-              </View>
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Cart"
-          component={CartScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View>
-                <Fontisto
-                  name="shopping-bag"
-                  size={22}
-                  color={focused ? "#C67C4E" : "black"}
-                />
-              </View>
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Notifications"
-          component={NotificationsScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View style={{ flex: 1, justifyContent: "center", alignItems: "center", width: 100, height: 100 }}>
-                <Ionicons
-                  name="notifications"
-                  size={28}
-                  color={focused ? "#C67C4E" : "black"}
-                />
-                {/* {focused && <View style={{ width: 10, height: 5, backgroundColor: "#000" }}></View>} */}
-              </View>
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    loading ? (<Splash/>) : (
+      <NavigationContainer>
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View>
+              <Entypo
+                name="home"
+                size={24}
+                color={focused ? "#C67C4E" : "black"}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View>
+              <Ionicons
+                name="heart"
+                size={24}
+                color={focused ? "#C67C4E" : "black"}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View>
+              <Fontisto
+                name="shopping-bag"
+                size={22}
+                color={focused ? "#C67C4E" : "black"}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                width: 100,
+                height: 100,
+              }}
+            >
+              <Ionicons
+                name="notifications"
+                size={28}
+                color={focused ? "#C67C4E" : "black"}
+              />
+              {/* {focused && <View style={{ width: 10, height: 5, backgroundColor: "#000" }}></View>} */}
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  </NavigationContainer>
+    )
   );
 }
--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import { height, width } from '../../utils/constants';
+import React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { height, width } from "../../utils/constants";
 
 const styles = StyleSheet.create({
-    container:{
-        width: width,
-        height: height,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
-    },
-    getStarted: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        margin: 10,
-        width: "80%",
-        height: 80,
-        backgroundColor:"#C67C4E"
-    },
-    image:{
-        width: 200,
-        height: 200,
-        resizeMode: 'contain'
-    }
-})
-const SplashScreen = () => {
+  container: {
+    width: width,
+    height: height,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  getStarted: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: 10,
+    width: "80%",
+    height: 80,
+    backgroundColor: "#C67C4E",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+});
+const Splash = () => {
   return (
     <View styles={styles.container}>
-      <Text></Text>
+      <Text>Splash Screen</Text>
     </View>
   );
-}
+};
 
-export default SplashScreen;
+export default Splash;
