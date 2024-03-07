479363a195a80aa8897fb3f60400b69de0e4e6d5
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-03-07 13:05:13 +0200

    feat: initialize the bottom tab

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { Text, View } from "react-native";
 import { useCallback } from "react";
+import {NavigationContainer} from "@react-navigation/native"
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {
   NotificationsScreen,
   SettingsScreen,
@@ -10,6 +12,8 @@ import {
   CartScreen,
 } from "./src/screens";
 SplashScreen.preventAutoHideAsync();
+
+const Tab = createBottomTabNavigator();
 
 // const Tab = createBottom
 export default function App() {
@@ -27,9 +31,29 @@ export default function App() {
     return null;
   }
 
+  const screenOptions = {
+    tabBarShowLabel: false,
+    headerShown: false,
+    tabBarStyles:{
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      height: 50,
+      backgroundColor: 'white',
+      elevation: 0,
+      right:0,
+      left:0,
+
+    }
+  }
   return (
-    <>
-      <HomeScreen />
-    </>
+    <NavigationContainer screenOptions={screenOptions}>
+      <Tab.Navigator default="Home">
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Favorites" component={FavoritesScreen}/>
+        <Tab.Screen name="Cart" component={CartScreen}/>
+        <Tab.Screen name="Notifications" component={NotificationsScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
